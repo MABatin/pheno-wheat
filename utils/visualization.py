f155c1a74b764e5d17f53116e0997d643989f67f
MABatin <leonhsn18@gmail.com>
2023-02-25 22:06:26 +0600

    update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,4 +1,6 @@
 import itertools
+import traceback
+
 import numpy as np
 import mmcv
 import torch
@@ -6,6 +8,7 @@ from PIL.Image import Image
 from torchvision.io import read_image
 import pycocotools.mask as maskUtils
 from mmdet.core import mask2ndarray, BitmapMasks
+from mmdet.datasets import build_dataset
 from torchvision.utils import draw_segmentation_masks
 import torchvision.transforms as T
 import os
@@ -17,6 +20,7 @@ from typing import List, Optional, Tuple, Union
 COLORS = [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230),
           (106, 0, 228), (0, 60, 100), (0, 80, 100), (0, 0, 70),
           (0, 0, 192), (250, 170, 30)]
+DEVICE = 'cpu'
 
 
 def draw_bounding_boxes_new(
@@ -157,120 +161,132 @@ def poly2mask(mask_ann, img_h, img_w):
     return mask
 
 
-def draw_bbox_masks(cfg, dataset, save_dir, split=None, outputs=None, show_score_thr=0):
+def draw_bbox_masks(cfg, dataset=None, save_dir=str, split=None, outputs=None, show_score_thr=0):
     mmcv.mkdir_or_exist(save_dir)
-    class_names = dataset.CLASSES
 
-    if outputs is None and split is not None:
-        if split == 'val':
-            try:
-                img_filepath = cfg.data.val.img_prefix
-            except:
-                print(f'val set does not exist in dataset')
-        img_filepath = cfg.data[split].img_prefix
-        for idx in range(len(dataset)):
-            image_name = dataset.data_infos[idx]['filename']
-            image_path = os.path.join(img_filepath, image_name)
-            img = read_image(image_path)
-            bboxes = torch.from_numpy(dataset.get_ann_info(idx)['bboxes']).to(device='cuda', dtype=torch.float32)
-            img_h = dataset.data_infos[idx]['height']
-            img_w = dataset.data_infos[idx]['width']
-            masks = dataset.get_ann_info(idx).get('masks')
-            mask_colors = ["blue"] * masks.shape[0]
-            if any(masks):
-                masks = BitmapMasks(
-                    [poly2mask(mask, img_h, img_w) for mask in masks], img_h, img_w)
-                masks = mask2ndarray(masks)
-                masks = torch.from_numpy(masks).to(device='cuda', dtype=torch.bool)
-                img = draw_segmentation_masks(img, masks, alpha=0.5, colors=mask_colors)
-                img = T.ToPILImage()(img)
-                save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_mask.png')
-                save_path = os.path.join(save_dir, save_name)
-                img.save(save_path)
-            bbox_colors = ["blue"] * bboxes.shape[0]
-            img = draw_bounding_boxes_new(img, bboxes, width=5, colors=bbox_colors)
-            img = T.ToPILImage()(img)
-            save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_mask.png')
-            save_path = os.path.join(save_dir, save_name)
-            img.save(save_path)
-    elif outputs is not None:
-        assert split is None, \
-            f'split needs to be None for drawing detected bboxes and masks'
-        img_filepath = cfg.data.test.img_prefix
-        if isinstance(outputs[0], tuple):
-            print(f"\nDrawing detected masks over {len(outputs)} images..........")
-            progress_bar = mmcv.ProgressBar(len(outputs))
-            for idx, output in enumerate(outputs):
+    try:
+        if outputs is None and split is not None:
+            if split == 'val':
+                try:
+                    img_filepath = cfg.data.val.img_prefix
+                except:
+                    print(f'val set does not exist in dataset')
+            dataset = build_dataset(cfg.data[split])
+            img_filepath = cfg.data[split].img_prefix
+            print(f"\nDrawing ground truth bboxes and masks over {len(dataset)} images..........")
+            progress_bar = mmcv.ProgressBar(len(dataset))
+            for idx in range(len(dataset)):
                 image_name = dataset.data_infos[idx]['filename']
                 image_path = os.path.join(img_filepath, image_name)
                 img = read_image(image_path)
+                bboxes = torch.from_numpy(dataset.get_ann_info(idx)['bboxes']).to(device=DEVICE, dtype=torch.float32)
                 img_h = dataset.data_infos[idx]['height']
                 img_w = dataset.data_infos[idx]['width']
-                # bboxes = torch.from_numpy(output[0][:, :4]).to(device='cuda', dtype=torch.float32)
-                bbox_results, segm_results = output
-                for i, (bbox_result, mask_results) in enumerate(zip(bbox_results, segm_results)):
-                    scores = torch.from_numpy(bbox_result[:, 4]).to(device='cuda', dtype=torch.float32)
-                    bboxes = torch.from_numpy(bbox_result[:, :4]).to(device='cuda', dtype=torch.float32)
-                    labels = [class_names[i]] * bboxes.shape[0]
+                masks = dataset.get_ann_info(idx).get('masks')
+                if any(masks):
                     masks = BitmapMasks(
-                        [maskUtils.decode(mask) for mask in mask_results], img_h, img_w)
+                        [poly2mask(mask, img_h, img_w) for mask in masks], img_h, img_w)
                     masks = mask2ndarray(masks)
-                    masks = torch.from_numpy(masks).to(device='cuda', dtype=torch.bool)
-                    if show_score_thr > 0:
-                        assert bboxes is not None and bbox_result.shape[1] == 5
-                        inds = scores > show_score_thr
-                        bboxes = bboxes[inds, :]
-                        masks = masks[inds, :]
-                        scores = scores[inds]
-                        inds_list = inds.tolist()
-                        labels = [labels[i] for i in range(len(labels)) if inds_list[i]]
-                    labels = [f'{label}|{scores[j]:.4f}' for j, label in enumerate(labels)]
-                    if len(class_names) == 1:
-                        y_cycle = itertools.cycle(COLORS)
-                        mask_colors = []
-                        for j in range(masks.shape[0]):
-                            mask_colors.append(next(y_cycle))
-                    else:
-                        mask_colors = [dataset.PALETTE[i]] * masks.shape[0]
-                    bbox_colors = [(240, 10, 10)] * bboxes.shape[0]
+                    masks = torch.from_numpy(masks).to(device=DEVICE, dtype=torch.bool)
+                    mask_colors = ["blue"] * masks.shape[0]
                     img = draw_segmentation_masks(img, masks, alpha=0.5, colors=mask_colors)
-                    img = draw_bounding_boxes_new(img, bboxes, width=5, labels=labels, colors=bbox_colors,
-                                                  font="/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-Regular.ttf",
-                                                  font_size=10)
+                    # img = T.ToPILImage()(img)
+                    # save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_mask.png')
+                    # save_path = os.path.join(save_dir, save_name)
+                    # img.save(save_path)
+                bbox_colors = ["blue"] * bboxes.shape[0]
+                img = draw_bounding_boxes_new(img, bboxes, width=5, colors=bbox_colors)
                 img = T.ToPILImage()(img)
                 save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_mask.png')
                 save_path = os.path.join(save_dir, save_name)
                 img.save(save_path)
 
                 progress_bar.update()
+        elif outputs is not None:
+            assert split is None, \
+                f'split needs to be None for drawing detected bboxes and masks'
+            assert dataset is not None, \
+                f'dataset cannot be None for drawing detected bboxes and masks'
+            img_filepath = cfg.data.test.img_prefix
+            class_names = dataset.CLASSES
+            if isinstance(outputs[0], tuple):
+                print(f"\nDrawing detected masks over {len(outputs)} images..........")
+                progress_bar = mmcv.ProgressBar(len(outputs))
+                for idx, output in enumerate(outputs):
+                    image_name = dataset.data_infos[idx]['filename']
+                    image_path = os.path.join(img_filepath, image_name)
+                    img = read_image(image_path)
+                    img_h = dataset.data_infos[idx]['height']
+                    img_w = dataset.data_infos[idx]['width']
+                    # bboxes = torch.from_numpy(output[0][:, :4]).to(device=DEVICE, dtype=torch.float32)
+                    bbox_results, segm_results = output
+                    for i, (bbox_result, mask_results) in enumerate(zip(bbox_results, segm_results)):
+                        scores = torch.from_numpy(bbox_result[:, 4]).to(device=DEVICE, dtype=torch.float32)
+                        bboxes = torch.from_numpy(bbox_result[:, :4]).to(device=DEVICE, dtype=torch.float32)
+                        labels = [class_names[i]] * bboxes.shape[0]
+                        masks = BitmapMasks(
+                            [maskUtils.decode(mask) for mask in mask_results], img_h, img_w)
+                        masks = mask2ndarray(masks)
+                        masks = torch.from_numpy(masks).to(device=DEVICE, dtype=torch.bool)
+                        if show_score_thr > 0:
+                            assert bboxes is not None and bbox_result.shape[1] == 5
+                            inds = scores > show_score_thr
+                            bboxes = bboxes[inds, :]
+                            masks = masks[inds, :]
+                            scores = scores[inds]
+                            inds_list = inds.tolist()
+                            labels = [labels[i] for i in range(len(labels)) if inds_list[i]]
+                        labels = [f'{label}|{scores[j]:.4f}' for j, label in enumerate(labels)]
+                        if len(class_names) == 1:
+                            y_cycle = itertools.cycle(COLORS)
+                            mask_colors = []
+                            for j in range(masks.shape[0]):
+                                mask_colors.append(next(y_cycle))
+                        else:
+                            mask_colors = [dataset.PALETTE[i]] * masks.shape[0]
+                        bbox_colors = [(240, 10, 10)] * bboxes.shape[0]
+                        img = draw_segmentation_masks(img, masks, alpha=0.5, colors=mask_colors)
+                        img = draw_bounding_boxes_new(img, bboxes, width=5, labels=labels, colors=bbox_colors,
+                                                      font="/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-Regular.ttf",
+                                                      font_size=10)
+                    img = T.ToPILImage()(img)
+                    save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_mask.png')
+                    save_path = os.path.join(save_dir, save_name)
+                    img.save(save_path)
 
-        elif isinstance(outputs[0], list):
-            print(f"\nDrawing detected bounding boxes over {len(outputs)} images..........")
-            progress_bar = mmcv.ProgressBar(len(outputs))
-            for idx, output in enumerate(outputs):
-                image_name = dataset.data_infos[idx]['filename']
-                image_path = os.path.join(img_filepath, image_name)
-                img = read_image(image_path)
-                for i, bbox_results in enumerate(output):
-                    scores = torch.from_numpy(bbox_results[:, 4]).to(device='cuda', dtype=torch.float32)
-                    bboxes = torch.from_numpy(bbox_results[:, :4]).to(device='cuda', dtype=torch.float32)
-                    labels = [class_names[i]] * bboxes.shape[0]
-                    bbox_colors = [(240, 10, 10)] * bboxes.shape[0]
-                    # colors = [dataset.PALETTE[i]] * bboxes.shape[0]
-                    if show_score_thr > 0:
-                        assert bboxes is not None and bbox_results.shape[1] == 5
-                        inds = scores > show_score_thr
-                        bboxes = bboxes[inds, :]
-                        scores = scores[inds]
-                        inds_list = inds.tolist()
-                        labels = [labels[i] for i in range(len(labels)) if inds_list[i]]
-                    labels = [f'{label}|{scores[j]:.4f}' for j, label in enumerate(labels)]
-                    img = draw_bounding_boxes_new(img, bboxes, width=5, labels=labels, colors=bbox_colors,
-                                                  font="/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-Regular.ttf",
-                                                  font_size=15)
-                img = T.ToPILImage()(img)
-                save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_bbox.png')
-                save_path = os.path.join(save_dir, save_name)
-                img.save(save_path)
+                    progress_bar.update()
 
-                progress_bar.update()
+            elif isinstance(outputs[0], list):
+                print(f"\nDrawing detected bounding boxes over {len(outputs)} images..........")
+                progress_bar = mmcv.ProgressBar(len(outputs))
+                for idx, output in enumerate(outputs):
+                    image_name = dataset.data_infos[idx]['filename']
+                    image_path = os.path.join(img_filepath, image_name)
+                    img = read_image(image_path)
+                    for i, bbox_results in enumerate(output):
+                        scores = torch.from_numpy(bbox_results[:, 4]).to(device=DEVICE, dtype=torch.float32)
+                        bboxes = torch.from_numpy(bbox_results[:, :4]).to(device=DEVICE, dtype=torch.float32)
+                        labels = [class_names[i]] * bboxes.shape[0]
+                        bbox_colors = [(240, 10, 10)] * bboxes.shape[0]
+                        # colors = [dataset.PALETTE[i]] * bboxes.shape[0]
+                        if show_score_thr > 0:
+                            assert bboxes is not None and bbox_results.shape[1] == 5
+                            inds = scores > show_score_thr
+                            bboxes = bboxes[inds, :]
+                            scores = scores[inds]
+                            inds_list = inds.tolist()
+                            labels = [labels[i] for i in range(len(labels)) if inds_list[i]]
+                        labels = [f'{label}|{scores[j]:.4f}' for j, label in enumerate(labels)]
+                        img = draw_bounding_boxes_new(img, bboxes, width=5, labels=labels, colors=bbox_colors,
+                                                      font="/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-Regular.ttf",
+                                                      font_size=15)
+                    img = T.ToPILImage()(img)
+                    save_name = dataset.data_infos[idx]['filename'].replace('.jpg', '_bbox.png')
+                    save_path = os.path.join(save_dir, save_name)
+                    img.save(save_path)
+
+                    progress_bar.update()
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+        print("Both output and split can't be None")
